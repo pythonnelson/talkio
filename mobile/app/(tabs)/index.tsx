ce28ffb57db390f78fc84d14cad8c89356b9d2cd
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2026-01-30 21:58:30 +0100

    feat: Implement the chats tab

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,15 +1,100 @@
+import ChatItem from "@/components/ChatItem";
+import EmptyUI from "@/components/EmptyUI";
+import { useChats } from "@/hooks/useChats";
+import { Chat } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
-import { ScrollView, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 
 const ChatsTab = () => {
+  const router = useRouter();
+  const { data: chats, isLoading, error } = useChats();
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 bg-surface items-center justify-center">
+        <ActivityIndicator size={"large"} color={"#1877F2"} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View className="flex-1 bg-surface items-center justify-center">
+        <Text className="text-red-500">Failed to load chats</Text>
+      </View>
+    );
+  }
+
+  // TODO: Test later
+  const handlePressItem = (chat: Chat) => {
+    router.push({
+      pathname: "/chat/[id]",
+      params: {
+        id: chat._id,
+        participantId: chat.participant._id,
+        name: chat.participant.name,
+        avatar: chat.participant.avatar,
+      },
+    });
+  };
+
   return (
-    <SafeAreaView className="bg-surface flex-1">
-      <ScrollView>
-        <Text className="text-white">Chats Tab</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <View className="bg-surface flex-1">
+      <FlatList
+        data={chats}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <ChatItem chat={item} onPress={() => handlePressItem(item)} />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{
+          paddingHorizontal: 20,
+          paddingTop: 16,
+          paddingBottom: 24,
+        }}
+        ListHeaderComponent={<Header />}
+        ListEmptyComponent={
+          <EmptyUI
+            title="No chats yet"
+            subtitle="Start a conversation!"
+            iconName="chatbubbles-outline"
+            iconColor="#6B6B70"
+            iconSize={64}
+            buttonLabel="New Chat"
+            onPressButton={() => console.log("New Chat button pressed")}
+          />
+        }
+      />
+    </View>
   );
 };
 
 export default ChatsTab;
+
+// Chat Header
+function Header() {
+  // const router = useRouter();
+
+  return (
+    <View className="px-5 pt-10 pb-4">
+      <View className="flex-row items-center justify-between">
+        <Text className="text-2xl font-bold text-foreground">Chats</Text>
+        <Pressable
+          className="size-10 bg-primary rounded-full items-center justify-center"
+          // onPress={() => router.push("/new-chat")}
+        >
+          <Ionicons name="create-outline" size={20} color={"#0D0D0F"} />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
---
+++ mobile/app/chat/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ChatDetailScreen = () => {
+  return (
+    <View>
+      <Text>Chat Detail Screen</Text>
+    </View>
+  );
+};
+
+export default ChatDetailScreen;
